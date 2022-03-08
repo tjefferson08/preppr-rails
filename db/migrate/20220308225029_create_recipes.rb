a222ec57d44da40ff6d36fa9e29153d506095f55
Travis Jefferson <travis@pathstream.com>
2022-03-08 17:52:39 -0500

    rails g scaffold for recipe model

---
+++ db/migrate/20220308225029_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :description
+      t.text :instructions
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
