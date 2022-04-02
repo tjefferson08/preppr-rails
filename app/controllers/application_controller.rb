8d9934224c7053bd9228105d2d4109a326523732
Travis Jefferson <tjefferson@meritamerica.org>
2022-04-02 13:55:58 -0400

    Basic authn mechanism leveraging cloudflare access header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_account!
+
+  Account = Struct.new(:key, keyword_init: true)
+
+  def authenticate_account!
+    raise "No authenticated account!" unless current_account.key.present?
+  end
+
+  def current_account
+    @current_account ||=
+      begin
+        return Account.new(key: "#{Rails.env}-#{request.host}") unless Rails.env.production?
+        Account.new(key: request.get_header("Cf-Access-Authenticated-User-Email"))
+      end
+  end
+
+  helper_method :current_account
 end
