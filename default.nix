8800f68f5e3d08a93054875e519c533c2fbebe18
Travis Jefferson <traviscjefferson@gmail.com>
2023-03-03 11:37:22 -0500

    Ran rails new!
    rails new preppr-rails \
        -d sqlite3 \
        --skip-action-mailbox \
        --skip-action-cable \
        --asset-pipeline propshaft \
        --skip-jbuilder \
        --js importmap \
        -c tailwind

---
+++ default.nix
@@ -0,0 +1,17 @@
+# latest commit on nixpkgs-unstable branch
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/52dc75a4fee3fdbcb792cb6fba009876b912bfe0.tar.gz") {} }:
+
+# pkgs.dockerTools.buildImage {
+#   name = "preppr-rails-nix-docker";
+#   config = {
+#     Cmd = [ "bin/rails routes" ];
+#   };
+# }
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.ruby_3_1
+    pkgs.sqlite
+    pkgs.tzdata
+  ];
+}
